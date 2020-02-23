761a991e83e54eda2288feca6d9d0f23de98611d
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-02-23 22:56:57 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,11 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.sentence?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.question?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
